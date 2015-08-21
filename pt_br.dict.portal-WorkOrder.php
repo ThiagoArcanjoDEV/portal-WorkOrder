1a3e11f2ebe7283035f3c7a066e28d0c68d9bbe8
Thiago Arcanjo <thiagoarcanjo@users.noreply.github.com>
2015-08-21 07:46:22 -0300

    Create pt_br.dict.portal-WorkOrder.php

---
+++ pt_br.dict.portal-WorkOrder.php
@@ -0,0 +1,14 @@
+<?php
+/**
+ * Dictionary file - PT-BR (Portuguese Brazilian)
+ *
+ * @copyright   Copyright (C) 2015 ThiagoArcanjo
+ * @license     http://opensource.org/licenses/AGPL-3.0
+ */
+
+Dict::Add('PT BR', 'Brazilian', 'Brazilian', array(
+	// Dictionary entries go here
+		'Portal:ShowWorkOrder' => 'Mostrar Ordens de ServiÃ§o',
+		'Portal:ListWorkOrder' => 'Ordens de ServiÃ§o',
+));
+?>
