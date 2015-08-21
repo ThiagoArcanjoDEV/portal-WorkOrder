56e1200a4d545514863799dc60be023fa7becdb6
Thiago Arcanjo <thiagoarcanjo@users.noreply.github.com>
2015-08-21 07:48:17 -0300

    Create en.dict.portal-WorkOrder.php

---
+++ en.dict.portal-WorkOrder.php
@@ -0,0 +1,15 @@
+<?php
+/**
+ * Dictionary file - EN (English)
+ *
+ * @copyright   Copyright (C) 2015 ThiagoArcanjo
+ * @license     http://opensource.org/licenses/AGPL-3.0
+ */
+
+Dict::Add('EN US', 'English', 'English', array(
+	// Dictionary entries go here
+        'Portal:ShowWorkOrder' => 'Show Work Orders',
+        'Portal:ListWorkOrder' => 'Work Orders',
+));
+
+?>
